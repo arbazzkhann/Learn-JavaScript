8381bbabf8a0709f26825fdd7cdbc50682d85694
Arbaz Khan <arbazkha78az@gmail.com>
2023-11-10 20:24:05 +0530

    tranfering prototype to oop

--- 27_OOP_Concepts/oop.js
+++ 27_OOP_Concepts/oop.js
@@ -45,3 +45,51 @@ const user2 = new user("ARBAZ", 665, "223")
 
 console.log(user1)
 console.log(user2)
+
+
+
+
+//YouTube Channel: Chai aur Code
+//Video Link: https://youtu.be/uMI5cNeHTOc
+
+/*****************************************************************/
+/*****************************************************************/
+
+// function chaiPrice(name, price) {
+//     this.name = name;
+//     this.price = price;
+// }
+
+
+// chaiPrice.prototype.increaseChaiPrice = function() {
+//     this.price++;
+//     console.log(`chai Price is ${this.price}`);
+// }
+
+// chaiPrice.prototype.printPrice = function() {
+//     console.log(`chai Price is ${this.price}`);
+// }
+
+// const chai = new chaiPrice("Madhav", 25)
+// const tea = new chaiPrice("Suman", 250)
+
+
+// chai.increaseChaiPrice()
+// tea.printPrice()
+
+
+
+
+/************* This prectice with new *************/
+function chaiUser(username, price) {
+    this.username = username;
+    this.price = price;
+}
+
+chaiUser.prototype.chaiPriceIncrease = function() {
+    console.log(`user name is ${this.username} and chai price is ${this.price}`)
+}
+
+const tea = new chaiUser("Arbaz", 500)
+
+tea.chaiPriceIncrease()
