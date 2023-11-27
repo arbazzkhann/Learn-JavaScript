18d049950d58a380d8577e890b09534637203e75
Arbaz Khan <arbazkha78az@gmail.com>
2023-11-27 21:09:11 +0530

    closures prectice

--- FOR PRECTICE/script.js
+++ FOR PRECTICE/script.js
@@ -1,39 +1,14 @@
-// /**
-//  * @return {null|boolean|number|string|Array|Object}
-//  */
-
-// let nums = [null, {}, 3]; 
-
-// Array.prototype.last = function(arr) {
-
-//     if(arr.lenth != null) {
-//         let lastElement = arr.length - 1;
-//         return arr[lastElement]
-//     } else {
-//         return -1;
-//     }
-
-// };
-
-// console.log(Array.last(nums));
+//closures
 
 
-
-
-if (!Array.prototype.last) {
-    Array.prototype.last = function() {
-      if (this.length === 0) {
-        return -1;
-      } else {
-        return this[this.length - 1];
-      }
-    };
+function abcd() {
+  let x = 12;
+  return function() {
+    console.log(x);
   }
-  
-  // Example 1
-  let nums1 = [null, {}, 3];
-  console.log(nums1.last()); // Output: 3
-  
-  // Example 2
-  let nums2 = [];
-  console.log(nums2.last()); // Output: -1
+}
+
+let ans = abcd();   
+
+abcd();    //it will only call
+ans();     //it will print answer
