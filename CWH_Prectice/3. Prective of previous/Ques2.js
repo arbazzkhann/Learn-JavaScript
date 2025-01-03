8e94ae19b5a81e8816711ab9c16278189a4ebabd
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-03 13:48:54 +0530

    prectice question 2

---
+++ CWH_Prectice/3. Prective of previous/Ques2.js
@@ -0,0 +1,11 @@
+/*
+Ques. Use typeof operator to find the datatype of the string in last question.
+
+*/
+
+let username = "arbaz"
+let b = 1234;
+
+console.log(username + b);
+
+console.log(typeof(username + b));
