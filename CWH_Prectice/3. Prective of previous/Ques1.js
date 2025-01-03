901625b6627dfd4d3c147749678ec786ddc24e22
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-03 13:45:44 +0530

    prectice question 1

---
+++ CWH_Prectice/3. Prective of previous/Ques1.js
@@ -0,0 +1,11 @@
+/*
+Ques. create a variable of type string and try to add a number in it.
+
+*/
+
+let username = "arbaz"
+let b = 1234;
+
+console.log(username + b)
+
+console.log(typeof(username + b))
