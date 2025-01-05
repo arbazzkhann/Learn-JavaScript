3ae65973603eebe22f965b03c8c16dd4678b5c96
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-05 17:17:21 +0530

    LocalString

---
+++ CWH_Prectice/P/prep.js
@@ -0,0 +1,4 @@
+const digits = new Number(10000000);
+
+console.log(digits.toLocaleString('en-US'));  //Print number as follows on US Standards
+console.log(digits.toLocaleString('en-IN')); //Print Number as Indian Standards
