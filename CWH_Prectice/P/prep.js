4ab8b7a47da290b3a3382be75f4c80a4e6c82f5c
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-07 13:57:08 +0530

    hasOwnProperty in JavaScript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,5 +1,9 @@
+const propertObject = {
+    name: "super man",
+    id: "user122",
+    email: "superman@google.com",
+    isLoggedIn: true,
+}
 
-
-let myArr = ["Superman", "Batman", "Spiderman"];
-
-console.log(Array.from(myArr[1]))
+console.log(propertObject.hasOwnProperty("isLoggedIn")); //output => true
+console.log(propertObject.hasOwnProperty("isLoggedOut")); //output => false
