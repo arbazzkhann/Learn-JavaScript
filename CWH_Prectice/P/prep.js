8e9a1d5925b4396a0a2571ab5e256dd892f17d86
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-12 17:56:43 +0530

    precticing .map and converting array number elements to binary

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,30 +1,6 @@
-//reduce
+const myOriginalArray = [2,6,7,5,8,9,4];
 
-let myClassObjects = [
-    {
-        name: "English",
-        price: 579
-    },
-    {
-        name: "Hindi",
-        price: 189
-    },
-    {
-        name: "Maths",
-        price: 349
-    },
-    {
-        name: "Science",
-        price: 399
-    },
-    {
-        name: "Social Science",
-        price: 299
-    }
-]
+const influencedArray = myOriginalArray.map((x) => x.toString(2));
 
-const myFinalPrice = myClassObjects.reduce((accumulator, currentPrice) => {
-    return accumulator + currentPrice.price
-}, 0);
 
-console.log(myFinalPrice)
+console.log(influencedArray)
