f0e3f7c65f2df7ed4be688ccf1fe4855e83a20e0
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-12 11:39:26 +0530

    precticing .filter in js

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,5 +1,40 @@
-const myArr = ["arbaz", "farhan", "masum", "subhan"];
+//filter
 
-myArr.filter((a,b,c)=> {
-    console.log(c)
+let myClassObject = [
+    {
+        name: "Arvind",
+        class: 10,
+        rollNo: 1
+    },
+    {
+        name: "Vivek",
+        class: 11,
+        rollNo: 2
+    },
+    {
+        name: "Manish",
+        class: 9,
+        rollNo: 3
+    },
+    {
+        name: "Sourav",
+        class: 12,
+        rollNo: 4
+    },
+    {
+        name: "Saquib",
+        class: 9,
+        rollNo: 5
+    },
+    {
+        name: "Suman",
+        class: 10,
+        rollNo: 6
+    }
+]
+
+myClassObject.filter((i) => {
+    if(i.class == 9) {
+        console.log(i.name);
+    }
 })
