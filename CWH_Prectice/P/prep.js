b6c9d0d6a28d5dfd8b08deb1540ce7123183ca5d
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-13 17:02:16 +0530

    final background changing prectice project making

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,11 +1,17 @@
 const red = document.querySelector('.red');
 const yellow = document.querySelector('.yellow');
 const orange = document.querySelector('.orange');
-const violet = document.querySelector('.violet');
-
-const redButton = document.querySelector('#redButton');
-
+const grey = document.querySelector('.grey');
 
 red.addEventListener('click', () => {
     document.body.style.backgroundColor = "red";
+})
+yellow.addEventListener('click', () => {
+    document.body.style.backgroundColor = "yellow";
+})
+orange.addEventListener('click', () => {
+    document.body.style.backgroundColor = "orange";
+})
+grey.addEventListener('click', () => {
+    document.body.style.backgroundColor = "grey";
 })
