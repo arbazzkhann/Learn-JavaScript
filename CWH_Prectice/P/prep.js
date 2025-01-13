22088323602105bd4962cc97f498075aabdf12e4
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-13 16:58:33 +0530

    making bgchanger project in simple js

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,20 +1,11 @@
-const mainHeading = document.querySelector('.mainHeading');
-const para1 = document.querySelector('.para1');
-const bulbDiv = document.querySelector('.bulbDiv');
-const switchButton = document.querySelector('.switchButton');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const orange = document.querySelector('.orange');
+const violet = document.querySelector('.violet');
+
+const redButton = document.querySelector('#redButton');
 
 
-switchButton.addEventListener('click', () => {
-    if(bulbDiv.innerText == "OFF") {
-        bulbDiv.style.backgroundColor = "yellow";
-        bulbDiv.innerText = "ON";
-    }
-    else if(bulbDiv.innerText == "ON") {
-        bulbDiv.style.backgroundColor = "white";
-        bulbDiv.innerText = "OFF";
-    }
+red.addEventListener('click', () => {
+    document.body.style.backgroundColor = "red";
 })
-
-
-    // bulbDiv.style.backgroundColor = "yellow"
-    // bulbDiv.innerText = "ON"
