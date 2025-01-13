54a9e539998201201b5ec2ba87fcd510adb43895
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-13 11:19:50 +0530

    precticing addEventlistener with DOM Manipulation

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,6 +1,20 @@
-const root = document.querySelector('#root');
+const mainHeading = document.querySelector('.mainHeading');
+const para1 = document.querySelector('.para1');
+const bulbDiv = document.querySelector('.bulbDiv');
+const switchButton = document.querySelector('.switchButton');
 
-const h1 = document.createElement('h1');
-h1.textContent = "Hello world i am from javascript.";
 
-root.append(h1)
+switchButton.addEventListener('click', () => {
+    if(bulbDiv.innerText == "OFF") {
+        bulbDiv.style.backgroundColor = "yellow";
+        bulbDiv.innerText = "ON";
+    }
+    else if(bulbDiv.innerText == "ON") {
+        bulbDiv.style.backgroundColor = "white";
+        bulbDiv.innerText = "OFF";
+    }
+})
+
+
+    // bulbDiv.style.backgroundColor = "yellow"
+    // bulbDiv.innerText = "ON"
