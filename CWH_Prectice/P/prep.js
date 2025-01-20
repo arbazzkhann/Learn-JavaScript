0525ebb7a52b53d603de25443be569e259819d0d
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-20 11:36:03 +0530

    for now final prectice of promise with then

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,19 +1,28 @@
-//promise
+//promise 2
 
 const myNewPromise = new Promise((res, rej) => {
-    if(true) {
-        return res("Resolve ho gaya.");
-    }
-    else {
-        return rej("Resolve nhi hua.");
-    }
+    return res("Sabse pehle ghar aao.");
 })
-
 
 myNewPromise
-.then((response)=> {
-    console.log(response);
+.then((data) => {
+    console.log(data);
+    return new Promise((res, rej) => {
+        return res("Gate kholo aur gate lagao.");
+    })
 })
-.catch((response) => {
-    console.log(response);
+.then((data) => {
+    console.log(data);
+    return new Promise((res, rej) => {
+        return res("Khana pakao, khana khao.");
+    })
+})
+.then((data) => {
+    console.log(data);
+    return new Promise((res, rej) => {
+        return res("Sojao kyunki tum thak chuke ho.");
+    })
+})
+.then((data) => {
+    console.log(data);
 })
