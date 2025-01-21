48300e85d29cc30eff9fcd15e3d781e64a5080a5
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-01-21 19:50:09 +0530

    precticing new keyword of instance in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,5 +1,6 @@
 //OOPs (Object Oriented Programming) Concept in JS
 
+//'new' keyword create new instance
 
 const myObj = {
     name: "Original Name",
@@ -17,6 +18,9 @@ const myObj = {
 }
 
 const user1 = new myObj.printDetails("Arbaz", 21, "delhi")
+ //'new' keyword create new instance that means 'new' creates new object in memory.
+ //in this example object of user1 and user2 are separated from each other.
 const user2 = new myObj.printDetails("Manav", 26, "Bihar")
 
-alert(`Hello ${user1.username}, you are from ${user1.state} and your age is ${user1.age}`)
+console.log(`Hello ${user1.username}, you are from ${user1.state} and your age is ${user1.age}`)
+console.log(`Hello ${user2.username}, you are from ${user2.state} and your age is ${user2.age}`)
