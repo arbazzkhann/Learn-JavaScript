37dbc62a90059851ed561a9630656f7ec6d459be
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-02-09 17:11:36 +0530

    Closures in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,26 +1,21 @@
-//OOPs (Object Oriented Programming) Concept in JS
+//Closures in js
 
-//'new' keyword create new instance
+/*
+    a function that has access to the variables and parameters 
+    of the function it's defined within
+*/
 
-const myObj = {
-    name: "Original Name",
-    age: 0,
-    state: 'delhi',
-
-    printDetails: function(username, age, state) {
-        this.username = username;
-        this.age = age;
-        this.state = state;
-
-        return this;
-
+function outer() {
+    var b = 10;
+    function a() {
+        
+        function c() {
+            console.log(b);
+        }
+        c();
     }
+    
+    a();
 }
 
-const user1 = new myObj.printDetails("Arbaz", 21, "delhi")
- //'new' keyword create new instance that means 'new' creates new object in memory.
- //in this example object of user1 and user2 are separated from each other.
-const user2 = new myObj.printDetails("Manav", 26, "Bihar")
-
-console.log(`Hello ${user1.username}, you are from ${user1.state} and your age is ${user1.age}`)
-console.log(`Hello ${user2.username}, you are from ${user2.state} and your age is ${user2.age}`)
+outer()
