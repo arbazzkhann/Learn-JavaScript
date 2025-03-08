71d90d481009d58e711011f8fc3dd3a080313bf0
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-08 13:33:14 +0530

    second way of finding number is prime or not

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -11,13 +11,17 @@ else {
         console.log("Enter Valid input, number should be in integer format.");
     }
     else {
-        for(let i = 2; i <= Math.floor(n/2); i += 2) {
-            if(n%i === 0) {
-                console.log("Prime nhi hai.");
-                break;
-            }
-            console.log("Prime hai.");
-        }
+        console.log(isPrime(n));    
     }
 }
 
+//Video link: https://youtu.be/H88hkdA02tY?t=3766
+function isPrime(n) {
+    if(n <= 1) return false;
+    if(n == 2) return true;
+    if(n%2 == 0) return false;
+    for(let i = 3; i <= Math.floor(n/2); i +=2 ) {
+        if(n%i == 0) return false;
+    }
+    return true;
+}
