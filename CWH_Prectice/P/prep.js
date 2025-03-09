4de931998f5fd9fd86621a36d6ee544f4073fa4f
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-09 21:11:36 +0530

    programme of strong number

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,16 +1,28 @@
-//Reversing the user input number
+//String number
 
-let input = prompt("Enter number");
+let input = prompt("Enter number: ");
+
+let copyInput = input;
 
 if(!isNaN(input)) {
-    let rev = 0;
+    let sum = 0;
     while(input > 0) {
+        let fact = 1;
         let rem = input % 10;
-        rev = rev * 10 + rem;
+        for(let i = 1; i <= rem; i++) {
+            fact *= i;
+        }
+        // sum = sum + fact;
+        sum += fact;
         input = Math.floor(input / 10);
+    }   
+    if(sum == copyInput) {
+        console.log(`${copyInput} is a strong number.`);
     }
-    console.log(rev);
+    else {
+        console.log(`Number is: ${copyInput} and sum: ${sum}, therefore its not a strong number.`);
+    }
 }
 else {
-    console.log("Enter valid input.");
+    console.log("Invalid input.");
 }
