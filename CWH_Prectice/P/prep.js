d0a0e0477647aa483306d79a7609eed4bbc920a8
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-19 13:17:33 +0530

    recursion in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,7 +1,13 @@
-function convertToArray() {
-    let argsArray = Array.from(arguments);
-    console.log(argsArray); // Output: [1, 2, 3, "hello"]
-  }
-  
-  convertToArray(1, 2, 3, "hello");
-  
+//recursion
+//when a function call itself
+
+function printNum(n) {
+    if(n > 10) {
+        console.log("Done!");
+        return;
+    }
+    console.log(n);
+    printNum(n+1);  //recursive call
+}
+
+printNum(1)
