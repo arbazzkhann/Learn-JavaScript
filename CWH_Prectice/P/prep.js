061ae111e3d9ffb13c1b998197b09ef98e0a6675
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-19 11:35:52 +0530

    finding sum of number with arguments object with for-loop

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,11 +1,11 @@
 function showArguments() {
     let sum = 0;
-    for(let val of arguments) {
-        sum += val;
+    for(let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
     }
     return sum;
 }
 
-let sum = showArguments(2,3,4,5, 100);
+let sum = showArguments(2,3,4,5);
 
 console.log(sum);
