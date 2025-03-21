1fb09041898224e788e51efeb4c24bfa1641cab9
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-21 17:14:53 +0530

    variables naming convension in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,22 +1,18 @@
-//global scope
-let hello = "hello world";
-{
-    console.log(hello); //it can be access anywhere in the code;
-}
+//Naming prectice of variables in javascript
+
+//1. Allowed or good naming prectice
+let name = "Arbaz"; // always use variable names with meaning  
+let name2 = "Arbaz 2"; //
+let name_3 = "Arbaz 3";
+let $name = "Arbaz 4";
+let _name = "Arbaz 5";
+
+//declare const variable as CAPITALIZE
+const NAME = "Arbaz 6";
 
 
-// block scope
-{
-    const myVal = 10;
-}
 
-// console.log(myVal) //referenceError
-
-//function scope
-function myName() {
-    var myNameVar = "Arbaz";
-}
-
-// console.log(myNameVar) //referenceError
-
-
+//2. Worst or Bad naming prectice
+// let 2Name = "Arbaz"; // syntax error
+// let x = "no name"; // no any meaning
+// let var = 50; // not allowed - syntax error (cannot use reserved keywords like function, var, for, let etc.);
