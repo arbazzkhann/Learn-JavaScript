d4b71c093a28579a18c99aa446d55d3708cf8e59
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-21 20:52:48 +0530

    Object.hasOwnProperty.call(objectName, propertyName)

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,28 +1,8 @@
-//object
-//Object are the premitive data type in javascript
-//Object are key-value pairs
-
-//creating object
 let myObj = {
-    name: "arbaz",
-    age: 21
-};
-console.log('Object1: ', myObj);
+    name: "arbaz"
+}
 
-//adding new key-value pair
-myObj.city = "new delhi";
-console.log('Object2: ', myObj);
+console.log(Object.prototype.hasOwnProperty.call(myObj, 'age'));
 
-//deleting key-value pair
-delete myObj.city;
-console.log('Object3: ', myObj);
-
-//updating value
 myObj.age = 22;
-console.log('Object4: ', myObj);
-
-//accessing with dot(.) notation
-console.log(myObj.name);
-
-//accessing as like array
-console.log(myObj['age']);
+console.log(Object.prototype.hasOwnProperty.call(myObj, 'age'));
