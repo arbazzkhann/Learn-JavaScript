44e8ff25f627507d31c5198eb867132bd6baac8a
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-21 17:49:13 +0530

    datatypes in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,14 +1,38 @@
-//lexical scope.
-//A function inside another function can access variables of their parents is known as lexical scope.
+//data types :
 
-function outer() {
-    let name = "Javascript";
+/* Learn as 'NNBBSSU'
+1. Number
+2. Null
+3. BigInt
+4. Boolean
+5. String
+6. Symbol
+7. Undefined
+*/
 
-    function inner() {
-        console.log(name);
-    }
+let number = 33;
+console.log(typeof(number)); //Number
 
-    inner();
-}
+let nullDataType = null; 
+console.log(typeof nullDataType); //object
+console.log(null instanceof Object); // false (null is NOT an actual object)
 
-outer();
+let bool = true;
+console.log(typeof bool); //boolean
+
+let bigInteger = 312312638127371238n;
+console.log(typeof bigInteger); //BigInt
+
+let nameString = "Hello";
+console.log(typeof nameString); //String
+
+let myId = Symbol("334");
+console.log(typeof myId); //Symbol
+let myId2 = Symbol("334");
+console.log(myId === myId2); //false
+
+let undefinedData = undefined;
+console.log(typeof undefinedData); //undefined
+let score; //not defined
+console.log(typeof score)
+
