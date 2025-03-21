412debf609e0ee8d0cab6a38297fa7d70fcbcafb
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-21 21:25:12 +0530

    JSON definition, useCase and Conversion

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,8 +1,41 @@
-let myObj = {
-    name: "arbaz"
-}
+//JSON (JavaScript Object Notation).
+//Lightweight data format.
+//It is easy for humans to read and write.
 
-console.log(Object.prototype.hasOwnProperty.call(myObj, 'age'));
 
-myObj.age = 22;
-console.log(Object.prototype.hasOwnProperty.call(myObj, 'age'));
+//JSON
+let myJSON = [
+    {
+        "name": "arbaz khan",
+        "age": 22,
+        "address": {
+            "city": "new delhi",
+            "pincode": "110091"
+        },
+        "phone": {
+            "first": "+91 87000 56482",
+            "second": "+91 97185 52231" 
+        }
+    }
+]
+
+//JSON CONVERSION :-
+
+//JSON.stringify
+let stringifyJSON = JSON.stringify(myJSON);
+
+console.log(stringifyJSON);
+console.log(typeof stringifyJSON); //string
+console.log(myJSON);
+console.log(typeof myJSON); //object
+
+
+
+//JSON.parse(objectName)
+let JSONData = '{"name": "arbaz", "age": 24}';
+
+let convertedJSON = JSON.parse(JSONData);
+
+
+console.log(JSONData.name)  //Output: 'undefined' because it treated as string not a object
+console.log(convertedJSON.name) // Output: arbaz
