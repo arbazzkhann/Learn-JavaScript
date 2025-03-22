8d7c2d9e809da7dd5230ffb086b195cef3740da0
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-03-22 16:36:42 +0530

    Object.is in JavaScript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,7 +1,3 @@
-let buffer = new ArrayBuffer(16);  //creates a 16-bytes buffer
+let val;
 
-let intArray = new Int32Array(buffer) //creates a typed Array from buffer
-
-intArray[0] = 42;
-console.log(intArray[9]) //undefined
-console.log(intArray[0]); //42
+console.log(Object.is(undefined, val))
