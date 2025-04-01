b529ce2ce9505788e3b4ec61ef2fc8c17a3d4f0d
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-01 20:41:17 +0530

    Set object in javascript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,3 +1,19 @@
-let val;
+//sets
+//it stores stores unique values
 
-console.log(Object.is(undefined, val))
+let set = new Set();
+
+set.add(10)
+set.add(10) //duplicate not stores
+set.add(10) //duplicate not stores
+set.add(20) 
+set.add(30) 
+set.add(40) 
+
+console.log(set);
+console.log(set.has(20));
+console.log(set.size);
+set.delete(40);
+console.log(set);
+
+
