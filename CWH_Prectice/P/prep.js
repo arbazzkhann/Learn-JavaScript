a2c5a75cbe0ea439ba38afa6202f16741b2437a4
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-04-02 18:22:23 +0530

    learning all about this keyword in JavaScript

--- CWH_Prectice/P/prep.js
+++ CWH_Prectice/P/prep.js
@@ -1,17 +1,88 @@
-//sets
-//it stores stores unique values
+//'this' keyword
+//'this' keyword in javascript refers to the object that is executing the current function.
+//its value depends on how and where the function called.
 
-let set = new Set();
+//1. 'this' in global scope (using it alone)
+console.log(this)
+//in browser 'this' refers to the 'window' object.
+//in node environment 'this' refers to the global scope.
+//in strict mode 'this' refers to the undefined.
 
-set.add(10)
-set.add(10) //duplicate not stores
-set.add(10) //duplicate not stores
-set.add(20) 
-set.add(30) 
-set.add(40) 
 
-console.log(set);  //prints whole set
-console.log(set.has(20)); //finding has 20
-console.log(set.size); //size of set
-set.delete(40); //deleting set value
-console.log(set); //prints whole set
+
+
+//2. 'this' in an object method
+// let name = 'madhar'
+const person = {
+    name: "John",
+    greet: function() {
+        console.log(this.name); //refers to the 'John'
+    }
+};
+person.greet(); //Output: John
+//this refers to the person object inside the method.
+
+
+
+
+//3. 'this' in a regular function
+//In a regular function, this depends on how the function is called
+function showThis() {
+    console.log(this);
+};
+showThis();
+//In browser refers to 'window' object.
+//In strict mode refers to 'undefined'
+
+
+
+
+//4. 'this' in an 'Event handler'
+//When we used in an event handler, 'this' refers to the element that triggered the event
+// document.getElementById('btn').addEventListener('click', function() {
+//     console.log(this); //it refers to the whole button element
+// });
+
+
+
+
+//5. 'this' in Arrow functions
+//In an Arrow function, this does not refer to the function itself.
+//Instead,  it inherits 'this' from th surrounding scope.
+
+const person2 = {
+    name: "Ali",
+    greet: () => {
+        console.log(this.name);
+    }
+};
+person2.greet(); //output: "global scope 'name' if existsn else 'undefined'"
+
+
+
+
+//6. 'this' in a constructor function
+//In a constructor function, 'this' refers to the newly created object
+function Person3(name) {
+    this.name = name;
+}
+
+const user1 = new Person3("John");
+console.log(user1.name); //Output: John
+//when using 'new', 'this' refers to the new object being created.
+
+
+
+
+//7. 'this' in a setTimeout and setInterval
+//In setTimeout and setInterval, 'this' usually refers to 'windows object'
+
+const obj = {
+    name: "test",
+    show: function() {
+        setTimeout(function() {
+            console.log(this.name);
+        });
+    }
+};
+obj.show(); //Output: "undefined" (or Window object in browser).
