64ce844700199fb32fdf9c12c179414d045df41e
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-10 18:45:09 +0530

    fetch using async/await

--- CWH_Prectice/P/script.js
+++ CWH_Prectice/P/script.js
@@ -1,9 +1,10 @@
-//Api: https://randomuser.me/api/
+const api = 'https://randomuser.me/api'
 
+// Fetch using async/await
 async function fetchData() {
-    const apiData = await fetch("https://randomuser.me/api/");
+    const apiData = await fetch(api);
     const finalData = await apiData.json();
     console.log(finalData)
 }
 
-fetchData()
+fetchData();
