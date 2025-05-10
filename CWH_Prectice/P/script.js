d94a6b6c22491459ba1a3210fd457f37c3de47bc
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-10 18:38:14 +0530

    fetch data with api

--- CWH_Prectice/P/script.js
+++ CWH_Prectice/P/script.js
@@ -1,1 +1,9 @@
-alert("Hello, World!");
+//Api: https://randomuser.me/api/
+
+async function fetchData() {
+    const apiData = await fetch("https://randomuser.me/api/");
+    const finalData = await apiData.json();
+    console.log(finalData)
+}
+
+fetchData()
