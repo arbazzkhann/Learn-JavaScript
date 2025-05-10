1e8e8c8db42536ba85dd30e8efffeb9f7f287dd9
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-10 19:27:08 +0530

    fetch using simple promises

--- CWH_Prectice/P/script.js
+++ CWH_Prectice/P/script.js
@@ -1,26 +1,7 @@
-const api = 'https://randomuser.me/api'
+const api = 'https://randomuser.me/api';
 
-// Fetch using async/await
-async function fetchData() {
-    const apiData = await fetch(api);
-    const finalData = await apiData.json();
-    console.log(finalData)
-}
-
-fetchData();
-
-
-//fetch using promises
-fetch(api)   // Make a GET request
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    return response.json();            // Convert response to JSON
-  })
-  .then(data => {
-    console.log(data);                // Handle the data
-  })
-  .catch(error => {
-    console.error('There was a problem with the fetch:', error);
-  });
+fetch(api)
+.then((data) => data.json())
+.then((data) => {
+  console.log(data) 
+})
