f7a994b51181e33ad41f0a04f43009de265a4987
Arbaz Khan <146950002+arbazzkhann@users.noreply.github.com>
2025-05-19 20:34:46 +0530

    Polymorphism in JS

--- 27_OOP_Concepts/OOPS By Anurag  (ProCodrr)/2. Factory Functions/script.js
+++ 27_OOP_Concepts/OOPS By Anurag  (ProCodrr)/2. Factory Functions/script.js
@@ -1,15 +1,17 @@
-function createdUser (firstName, lastName, age) {
+//Polimorphism
+function getAgeYear() {
+    return new Date().getFullYear() - this.age;
+}
+
+function createUser (firstName, lastName, age) {
     const user = {
         firstName,
         lastName,
         age,
-        getAgeYear () {
-            return new Date().getFullYear() - age;
-        }
+        getAgeYear
     }
     return user
 }
 
-const user1 = createdUser("Arbaz", "Khan", 22);
-
-
+const user1 = createUser("Arbaz", "Khan", 22);
+const user2 = createUser("Ravi", "Negi", 45);
