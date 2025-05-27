75dc574da637fae2cf2193431c166def62a95f87
Arbaz Khan <arbazkha78az@gmail.com>
2025-05-27 18:39:29 +0530

    fetch with async/await and display data on simple html card

--- CWH_Prectice/6. Fetch/script.js
+++ CWH_Prectice/6. Fetch/script.js
@@ -3,26 +3,53 @@ let cardName = document.querySelector('#cardName');
 let profession = document.querySelector('#profession');
 
 //Fetch with Promises
-fetch('https://randomuser.me/api/')
-  .then((response) => response.json())
-  .then((data) => {
-    const firstName = data.results[0]['name']['first'];
-    const lastName = data.results[0]['name']['last'];
+// fetch('https://randomuser.me/api/')
+//   .then((response) => response.json())
+//   .then((data) => {
+//     const firstName = data.results[0]['name']['first'];
+//     const lastName = data.results[0]['name']['last'];
 
-    //card picture
-    const image = data.results[0]['picture']['large'];
-    picture.setAttribute('src', image)
+//     //card picture
+//     const image = data.results[0]['picture']['large'];
+//     picture.setAttribute('src', image)
 
-    //card name
-    cardName.innerText = `${firstName} ${lastName}`;
+//     //card name
+//     cardName.innerText = `${firstName} ${lastName}`;
 
-    //card profession
-    const userProfession = data.results[0]['name']['last'];
-    profession.innerText = userProfession;
+//     //card profession
+//     const userProfession = data.results[0]['name']['last'];
+//     profession.innerText = userProfession;
 
 
-  })
-  .catch((error) => {
-    console.error('Error fetching user:', error);
-  });
+//   })
+//   .catch((error) => {
+//     console.error('Error fetching user:', error);
+//   });
 
+
+
+//with async await
+//Fetch with Promises
+
+async function randomUserApi() {
+  const fetchData = await fetch('https://randomuser.me/api/');
+  const finalData = await fetchData.json();
+
+  const firstName = finalData.results[0]['name']['first'];
+  const lastName = finalData.results[0]['name']['last'];
+
+  //card picture
+  const image = finalData.results[0]['picture']['large'];
+  picture.setAttribute('src', image)
+
+  //card name
+  cardName.innerText = `${firstName} ${lastName}`;
+
+  //card profession
+  const userProfession = finalData.results[0]['name']['last'];
+  profession.innerText = userProfession;
+  
+  
+}
+
+randomUserApi()
