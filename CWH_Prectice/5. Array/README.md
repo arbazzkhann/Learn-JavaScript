50f1650b580ae36aa29153113a438b0b2b045d9b
Arbaz Khan <arbazkha78az@gmail.com>
2025-05-29 12:31:04 +0530

    reversing array

--- CWH_Prectice/5. Array/README.md
+++ CWH_Prectice/5. Array/README.md
@@ -124,3 +124,16 @@ for(let i=0; i < myArr.length; i++) {
 console.log("Max: ", maxElement);
 console.log("Second Max: ", nextMaxElement);
 ```
+
+6. Revese Array?
+```js
+let myArr = [303, 45, 34, 5, 65, 643, 42, 24, 57, 43, 66, 44];
+
+let secondArr = [];
+
+for(let i = myArr.length - 1; i >= 0; i--) {
+    secondArr.push(myArr[i]);
+}
+
+console.log(secondArr)
+```
--- CWH_Prectice/5. Array/script.js
+++ CWH_Prectice/5. Array/script.js
@@ -1,17 +1,9 @@
 let myArr = [303, 45, 34, 5, 65, 643, 42, 24, 57, 43, 66, 44];
 
-let maxElement = -Infinity;
-let nextMaxElement = -Infinity;
+let secondArr = [];
 
-for(let i=0; i < myArr.length; i++) {
-    if(myArr[i] > maxElement) {
-        nextMaxElement = maxElement;
-        maxElement = myArr[i];
-    }
-    else if (myArr[i] > nextMaxElement && myArr[i] !== maxElement) {
-        nextMaxElement = myArr[i];
-    }
+for(let i = myArr.length - 1; i >= 0; i--) {
+    secondArr.push(myArr[i]);
 }
 
-console.log("Max: ", maxElement);
-console.log("Second Max: ", nextMaxElement);
+console.log(secondArr)
