b9b17a689b8b672b3d57e5c443c930accbe46a22
Arbaz Khan <arbazkha78az@gmail.com>
2025-05-29 12:21:02 +0530

    another way of findig second max element in array

--- CWH_Prectice/5. Array/script.js
+++ CWH_Prectice/5. Array/script.js
@@ -1,17 +1,17 @@
 let myArr = [303, 45, 34, 5, 65, 643, 42, 24, 57, 43, 66, 44];
 
-let maxElement = myArr[0];
-let secondMax = 0;
+let maxElement = -Infinity;
+let nextMaxElement = -Infinity;
 
-for (let i = 1; i < myArr.length; i++) {
-    if (myArr[i] > maxElement) {
-        secondMax = maxElement;
+for(let i=0; i < myArr.length; i++) {
+    if(myArr[i] > maxElement) {
+        nextMaxElement = maxElement;
         maxElement = myArr[i];
-    } 
-    // else if (myArr[i] > secondMax && myArr[i] !== maxElement) {
-    //     secondMax = myArr[i];
-    // }
+    }
+    else if (myArr[i] > nextMaxElement && myArr[i] !== maxElement) {
+        nextMaxElement = myArr[i];
+    }
 }
 
-console.log("Max:", maxElement);
-console.log("Second Max:", secondMax);
+console.log("Max: ", maxElement);
+console.log("Second Max: ", nextMaxElement);
