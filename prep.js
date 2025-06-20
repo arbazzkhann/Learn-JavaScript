e5bc4e50415028b83c0add5050b48b779b4831b0
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-20 20:41:14 +0530

    filter with array

--- prep.js
+++ prep.js
@@ -7,9 +7,17 @@ let myArr = [
     {firstName: "Suman", id: 102},
     {firstName: "Madhav", id: 103},
     {firstName: "Sanjay", id: 104}
-]
+];
 
 
-const firstNames = myArr.map((item) => item.id)
+const firstNames = myArr.map((item) => item.id);
 
 console.log(firstNames);
+
+
+//filter:
+const filterdArray = myArr.filter((item) => {
+    return (item.id > 102)
+});
+
+console.log(filterdArray);
