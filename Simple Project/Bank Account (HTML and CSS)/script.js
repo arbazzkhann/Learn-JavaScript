b17109576b354444524c0a5f1bf2c23be5f01a60
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-21 19:30:34 +0530

    simple html and js bank account creation and amount updation form project

---
+++ Simple Project/Bank Account (HTML and CSS)/script.js
@@ -0,0 +1,68 @@
+//Account creation input
+const accountCreationForm = document.querySelector('#accountCreationForm');
+const creationFullName = document.querySelector('#creationFullName');
+const creationAmount = document.querySelector('#creationAmount');
+
+//Account Update Input
+const accountUpdationForm = document.querySelector('#accountUpdationForm');
+const accountNumber = (document.querySelector('#accountNumber'));
+const updationAmount = document.querySelector('#updationAmount');
+const accountUpdationSelectionBox = document.querySelector('#accountUpdationSelectionBox');
+
+
+//Empty array for accounts
+let accounts = [];
+
+//Factory function for account
+const BankAccountCreationFactoryFunction = function (fullName, amount = 0) {
+    this.fullName = fullName;
+    this.amount = amount;
+    this.accountNumber = Date.now();
+
+    this.deposit = (amount) => {
+        this.amount += amount;
+    }
+
+    this.withdraw = (amount) => {
+        this.amount -= amount;
+    }
+}
+
+//account creation
+accountCreationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const account = new BankAccountCreationFactoryFunction(creationFullName.value, parseInt(creationAmount.value));
+    accounts.push(account);
+    console.log(accounts);
+    // console.log(accounts[0].accountNumber);
+});
+
+//account updation
+accountUpdationForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    accounts.map((item) => {
+        if(accountUpdationSelectionBox.value === "deposit") {
+            if(item.accountNumber == accountNumber.value) {
+            item.amount += +updationAmount.value;
+            console.log(accounts)
+            document.querySelector('.message').innerText = item.amount;
+            }
+            else {
+                // console.log("Account not found!");
+                document.querySelector('.message').innerText = "Account not found!";
+            }
+        } 
+        else if(accountUpdationSelectionBox.value === "withdraw") {
+            if(item.accountNumber == accountNumber.value) {
+                item.amount -= +updationAmount.value;
+                console.log(accounts)
+            }
+            else {
+                // console.log("Account not found!");
+                document.querySelector('.message').innerText = "Account not found!";
+            }
+        }
+    })
+})
