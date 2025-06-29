27aef01b9ad263bb1a53e361ccb076c495a53137
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-29 21:40:04 +0530

    private field in js

--- 27_OOP_Concepts/OOPS By Coder's Gyan/5. Encapsulation/script.js
+++ 27_OOP_Concepts/OOPS By Coder's Gyan/5. Encapsulation/script.js
@@ -1,15 +1,23 @@
 class BankAccountCreate {
+    customerName;
+    accountNumber;
+    #balance = 0;
+
     constructor(customerName, balance) {
         this.customerName = customerName;
-        this._balance = balance;
+        this.#balance = balance;
         this.accountNumber = Date.now();
     }
 
     deposit(amount) {
-        this._balance += amount;
+        this.#balance += amount;
     }
     withdraw(amount) {
-        this._balance -= amount;
+        this.#balance -= amount;
+    }
+
+    setBalance(amount) {
+        this.#balance = amount
     }
 }
 
@@ -17,5 +25,6 @@ const rakesh = new BankAccountCreate("rakesh", 3000);
 
 console.log(rakesh);
 
-rakesh._balance = "Suman";
+// rakesh.#balance = "Suman";  //only access within class
+rakesh.setBalance(340);
 console.log(rakesh);
