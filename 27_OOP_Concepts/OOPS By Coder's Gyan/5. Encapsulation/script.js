655fb47b10ed7c9962117494443a0f1884fbf996
Arbaz Khan <arbazkha78az@gmail.com>
2025-06-29 22:11:52 +0530

    encapsulation in js

--- 27_OOP_Concepts/OOPS By Coder's Gyan/5. Encapsulation/script.js
+++ 27_OOP_Concepts/OOPS By Coder's Gyan/5. Encapsulation/script.js
@@ -9,6 +9,7 @@ class BankAccountCreate {
         this.accountNumber = Date.now();
     }
 
+    //deposit and withdraw amount
     deposit(amount) {
         this.#balance += amount;
     }
@@ -16,14 +17,36 @@ class BankAccountCreate {
         this.#balance -= amount;
     }
 
-    setBalance(amount) {
+    //public methods
+    // setBalance(amount) {
+    //     if(isNaN(amount)) {
+    //         throw new Error('Amount is not a valid input.');
+    //     }
+    //     this.#balance = amount
+    // }
+    // getBalance() {
+    //     return this.#balance;
+    // }
+
+    //getter and setter
+    //accessing as normal property
+    set balance(amount) {
         if(isNaN(amount)) {
             throw new Error('Amount is not a valid input.');
         }
         this.#balance = amount
     }
-    getBalance() {
+    get balance() {
         return this.#balance;
+    }
+
+    //private method
+    #calculateInterest() {
+        console.log("Calculated interest is ", this.#balance/23);
+    }
+    //getter and setter for calculateInterest
+    get calculateInterest() {
+        this.#calculateInterest();
     }
 }
 
@@ -31,6 +54,17 @@ const rakesh = new BankAccountCreate("rakesh", 3000);
 
 console.log(rakesh);
 
-// rakesh.#balance = "Suman";  //only access within class
-rakesh.setBalance(340);
-console.log(rakesh.getBalance());
+//it will throw error
+//only access within class
+// rakesh.#balance = "Suman";
+
+//public methods
+// rakesh.setBalance(340);  //setting balance using public methods
+// console.log(rakesh.getBalance());  //using public methods
+
+//getter and setter
+rakesh.balance = 550;  //setting balance using setter
+console.log(rakesh.balance);  //using getter
+
+//getting private method
+rakesh.calculateInterest;
